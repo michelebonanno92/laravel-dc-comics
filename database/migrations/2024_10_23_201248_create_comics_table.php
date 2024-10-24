3caee4a57475380d1c4e560ae75b6af84874b9e9
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-24 08:29:36 +0200

    creato database , model e migration

---
+++ database/migrations/2024_10_23_201248_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',128);
+            $table->text('description');
+            $table->string('thumb', 1024)->nullable();
+            // nullable se non è obbligatorio
+            $table->decimal('price', 10, 2)->unsigned();
+            $table->string('series',64);
+            $table->date('sale_date')->nullable();
+            $table->string('type',64);
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
