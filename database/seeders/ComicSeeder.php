ad8adda23859658fbd661ba9678b3a3a5577ad67
micheleboanno92 <michelebonanno92@gmail.com>
2024-10-24 15:59:36 +0200

    riempito tabella con il seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Models 
+use App\Models\Comic;
+// collegamento al Model Comic
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -12,6 +16,37 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        Comic::truncate();
+
+        $comics = config('comics');
+        // ci prendiamo tutti i comics dal faile in config
+        foreach ( $comics as $singleComic) {
+            $comic = new Comic();
+            $comic->title = $singleComic['title'];
+            $comic->description = $singleComic['description'];
+            $comic->thumb = $singleComic['thumb'];
+            // dd($singleComic['thumb']);
+            $priceNumber = floatval(substr($singleComic['price'],1));
+            $comic->price =  $priceNumber;
+            // dd($priceNumber);
+            $comic->series = $singleComic['series'];
+            // dd($singleComic['series']);
+            $comic->sale_date = $singleComic['sale_date'];
+            $comic->type = $singleComic['type'];
+            // con il metodo json
+            $jsonArtists = json_encode($singleComic['artists']);
+            // dd($jsonArtists);
+            $comic->artists = $jsonArtists ;
+            $implodeWriters = implode('|', $singleComic['writers']);
+            $comic->writers = $implodeWriters;
+            $comic->save();
+            // dd($comic);
+
+
+
+
+
+        }
     }
 }
